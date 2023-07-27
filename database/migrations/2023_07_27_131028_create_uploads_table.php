498886b7505265e79b74018f79ca067a9b4f5645
soheylfarzane <solyfarzane@gmail.com>
2023-07-27 16:43:00 +0330

    uploads migration

---
+++ database/migrations/2023_07_27_131028_create_uploads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('uploads', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('template_id');
+            $table->string('path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('uploads');
+    }
+};
